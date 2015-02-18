b54a5d398aaed0a3eced0b7d5b762b01e739eefb
Andrew <akv9509@saintjoe.edu>
2015-02-18 12:41:08 -0500

    This answers the second question in the test with function declaration.

---
+++ number2.js
@@ -0,0 +1,23 @@
+//Andrew Klump
+//CMP237
+//Test 1
+//Feb 2015
+//(4 points) Write a program that shows both ways of declaring a function.  Be sure to actually somehow use both functions that you write.
+
+
+
+var longer  = function(){
+
+	console.log('This is the longer and more versitile declaration');
+
+}
+
+
+function shorter(){
+
+	console.log('This is the shorter more convienient declaration');
+}
+
+
+longer();
+shorter();
